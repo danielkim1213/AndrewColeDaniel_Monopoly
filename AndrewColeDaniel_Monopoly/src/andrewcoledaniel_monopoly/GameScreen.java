e326d9bc8369cf6593012e62172de98d43e10099
dakim0069 <dakim0069@8D1X182.Student.UGDSB.ED>
2022-01-21 10:18:52 -0500

    new commit

--- AndrewColeDaniel_Monopoly/src/andrewcoledaniel_monopoly/GameScreen.java
+++ AndrewColeDaniel_Monopoly/src/andrewcoledaniel_monopoly/GameScreen.java
@@ -30,20 +30,71 @@ public class GameScreen extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        pnlStatus = new javax.swing.JPanel();
+        lblTurn = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txaProperties = new javax.swing.JTextArea();
+        lblProperties = new javax.swing.JLabel();
+        btnBuyHouse = new javax.swing.JButton();
+        btnSellHouse = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(800, 600));
+        setResizable(false);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 250, Short.MAX_VALUE)
+        pnlStatus.setBackground(new java.awt.Color(204, 255, 204));
+
+        lblTurn.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        lblTurn.setText("Turn 1");
+
+        txaProperties.setColumns(20);
+        txaProperties.setRows(5);
+        txaProperties.setText("Boardwalk: \nPark place:\nWaterworks:");
+        jScrollPane1.setViewportView(txaProperties);
+
+        lblProperties.setBackground(new java.awt.Color(255, 255, 255));
+        lblProperties.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        lblProperties.setText("Your Properties");
+
+        btnBuyHouse.setText("Buy House");
+
+        btnSellHouse.setText("Sell House");
+
+        javax.swing.GroupLayout pnlStatusLayout = new javax.swing.GroupLayout(pnlStatus);
+        pnlStatus.setLayout(pnlStatusLayout);
+        pnlStatusLayout.setHorizontalGroup(
+            pnlStatusLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlStatusLayout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(lblTurn, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(91, 91, 91))
+            .addGroup(pnlStatusLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(pnlStatusLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1)
+                    .addGroup(pnlStatusLayout.createSequentialGroup()
+                        .addComponent(lblProperties)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(pnlStatusLayout.createSequentialGroup()
+                        .addComponent(btnBuyHouse, javax.swing.GroupLayout.DEFAULT_SIZE, 106, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnSellHouse, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap())
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+        pnlStatusLayout.setVerticalGroup(
+            pnlStatusLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(pnlStatusLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(lblTurn)
+                .addGap(29, 29, 29)
+                .addComponent(lblProperties)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(pnlStatusLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnBuyHouse, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSellHouse, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(252, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -51,20 +102,27 @@ public class GameScreen extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(0, 600, Short.MAX_VALUE)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(0, 664, Short.MAX_VALUE)
+                .addComponent(pnlStatus, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(pnlStatus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton btnBuyHouse;
+    private javax.swing.JButton btnSellHouse;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JLabel lblProperties;
+    private javax.swing.JLabel lblTurn;
+    private javax.swing.JPanel pnlStatus;
+    private javax.swing.JTextArea txaProperties;
     // End of variables declaration//GEN-END:variables
 }
--- AndrewColeDaniel_Monopoly/src/andrewcoledaniel_monopoly/MainMenu.java
+++ AndrewColeDaniel_Monopoly/src/andrewcoledaniel_monopoly/MainMenu.java
@@ -56,6 +56,7 @@ public class MainMenu extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
